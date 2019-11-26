2de9a17ebab8fef39e3e5f3316fa7cabe2269094
Adam <burns679@umn.edu>
2019-11-26 12:58:40 -0600

    Added scripts to extract and normalize the JSON for insertion into an SQL database

---
+++ extract.py
@@ -0,0 +1,55 @@
+import json
+import os
+from dotenv import load_dotenv
+from urllib.parse import urlencode
+from urllib.request import urlopen
+
+
+def load_key():
+    load_dotenv()
+    return ('hapikey', os.getenv('HUBSPOT_API_KEY'))
+
+
+def build_url(url_prefix, params, offset):
+    if offset:
+        params = [(val, key) for (val, key) in params if val != 'offset']
+        params.append(('offset', offset))
+    return url_prefix + urlencode(params)
+
+
+def download_data(url):
+    #TODO: Implement sleeping before getting  all of the data to prevent DDOS
+    with urlopen(url) as response:
+        data = json.loads(response.read())
+        return data
+
+
+def run(hasMore = True):
+    params = [('includeAssociations', 'true'), 
+              ('limit', '250'), 
+              ('properties', 'dealname'),
+              ('properties', 'dealstage'),
+              ('properties', 'createdate'), 
+              ('properties', 'closedate'),
+              ('properties', 'hubspot_owner_id'),
+              ('properties', 'hs_object_id'), 
+              ('properties', 'amount'),
+              ('properties', 'dealtype')
+    ]
+    params.append(load_key())
+    url_prefix = "https://api.hubapi.com/deals/v1/deal/paged?"
+    data = []
+    offset = None
+    while(hasMore):
+        temp_data = download_data(build_url(url_prefix, params, offset))
+        data += temp_data['deals']
+        hasMore = temp_data['hasMore']
+        print('hasMore: \t' + str(hasMore))
+        offset = temp_data['offset']
+        print('offset: \t' + str(offset))
+    print(len(data))
+    with open('df.json', 'w') as json_file:
+        json.dump(data, json_file)
+        print('We saved the file!')
+
+run()
---
+++ transform.py
@@ -0,0 +1,17 @@
+import json
+import pandas as pd
+from flatten_json import flatten
+
+def clean_data(loc):
+    PARAMS = ['portalId',
+              'dealId',
+              'properties_dealname_value',
+              'properties_hubspot_owner_id_value',
+              'properties_dealstage_value',
+              'properties_hs_object_id_value',
+              'properties_createdate_value',
+              'properties_amount_value',
+              'properties_dealtype_value',
+              'properties_closedate_value']
+    df = pd.DataFrame.from_dict([flatten(d) for d in open_data(loc)])
+    return df[PARAMS]
