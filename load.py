9d0cba122440271f60a01f32eb470a194b593c9b
Adam <burns679@umn.edu>
2019-12-24 15:32:56 -0600

    Data is now UPSERT'ed to remove the constant DROP'ing of tables

--- load.py
+++ load.py
@@ -14,4 +14,45 @@ def generate_engine()
 
 def load(data):
     engine = create_engine(generate(engine))
-    data.to_sql('Hubspot Deals', engine)
+    data.to_sql('Temp Hubspot Deals', engine)
+    upsert = 'INSERT INTO "Hubspot Deals"
+       (index,
+        portalId,
+        dealId,
+        properties_dealname_value,
+        properties_hubspot_owner_id_value,
+        properties_dealstage_value,
+        properties_hs_object_id_value,
+        properties_createdate_value,
+        properties_amount_value,
+        properties_dealtype_value,
+        propertie_closedate_value) AS A
+    SELECT
+        index,
+        portalId,
+        dealId,
+        properties_dealname_value,
+        properties_hubspot_owner_id_value,
+        properties_dealstage_value,
+        properties_hs_object_id_value,
+        properties_createdate_value,
+        properties_amount_value,
+        properties_dealtype_value,
+        properties_closedate_value
+    FROM
+        "Temp Hubspot Deals" AS B
+    ON CONFLICT (dealId) DO UPDATE SET
+    index = B.index,
+    portalId = B.portalId,
+    dealId = B.dealId,
+    properties_dealname_value = B.properties_dealname_value,
+    properties_hubspot_owner_id_value = B.properties_hubspot_owner_id_value,
+    properties_dealstage_value = B.properties_dealstage_value,
+    properties_hs_object_id_value = B.properties_hs_object_id_value,
+    properties_createdate_value = B.properties_createdate_value,
+    properties_amount_value = B.properties_amount_value,
+    properties_dealtype_value = B.properties_dealtype_value,
+    properties_closedate_value = B.properties_closedate_value;'
+    connection.execute(upsert)
+    drop = 'DROP TABLE "Temp Hubspot Deals";'
+    connection.execute(drop)
